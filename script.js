b7f952e9f826619756a1f9f67a80fbcc93ebd41e
igo <igor.paula7@outlook.com>
2023-03-02 10:38:09 -0300

    Adicionando Image Switcher

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const botao = document.querySelector(".switcher > button")
+const img = document.querySelector("#pic_and_name > img")
 
 botao.onclick = function switcher() {
   if (document.body.classList.contains("dark")) {
     document.body.classList.remove("dark")
+    img.setAttribute("src", "./assets/pic.png")
   } else {
     document.body.classList.add("dark")
+    img.setAttribute("src", "./assets/pic-dark-mode.png")
   }
 }
